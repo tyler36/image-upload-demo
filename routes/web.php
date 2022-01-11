b620c07282c16df045b58b1adc01b919af1af8b7
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-01-11 17:33:35 +0900

    application prep work

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
 
+Route::get('/', [ImageController::class, 'index']);
+Route::get('/images', [ImageController::class, 'show']);
+Route::post('/upload', [ImageController::class, 'store']);
