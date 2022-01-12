5fa28c43b2da33e1160cf76331b8e7137c0f01ad
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-01-12 09:55:14 +0900

    add `FilePond`

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## Setup
 
 - Based on [Advanced Image uploading with Vue & Laravel](https://laracasts.com/series/advanced-image-uploading-with-vue-and-laravel) Laracasts series
+
+- Use [FilePond](https://pqina.nl/filepond/) to power upload
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,16 +1,39 @@
 <template>
-    <div>
-        <h2 class="text-xl text-center">{{ message }}</h2>
+    <div class="mt-4">
+        <file-pond
+            name="image"
+            ref="pond"
+            label-idle="Click or drag image here"
+            server="/upload"
+            @init="filepondInitialized"
+            accepted-file-types="image/*"
+        />
     </div>
 </template>
 
 
 <script>
-    export default {
-        data() {
-            return {
-                message: 'Hello world!'
-            };
+// Import FilePond
+import vueFilePond from 'vue-filepond';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import "filepond/dist/filepond.min.css";
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+export default {
+    components: {
+        FilePond
+    },
+
+    data() {
+        return {};
+    },
+
+    methods: {
+        filepondInitialized() {
+            console.log('File pond is ready ...');
+            console.log('Filepond object: ', this.$refs.pond);
         }
-    };
+    }
+};
 </script>
